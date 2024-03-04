d4ca32729163a3e7c00572f1885e2b5141c9ba75
Samuel Falcón Alonso <samu.gc98r@gmail.com>
2024-03-04 09:37:05 +0000

    GENERAL - Connection to DB done

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const sequelize = require('./db')
 
 const app = express()
 
 app.use(morgan('dev'))
 app.use(express.json())
 
+const checkDb = async () => {
+  try {
+    await sequelize.authenticate()
+    console.log('Connected to NebulaNova')
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
 app.get('/', (req, res) => res.send('Connected to NebulaNova API'))
 
-app.listen(process.env.PORT, (err) => {
+app.listen(process.env.PORT, async (err) => {
   if (err) throw new Error(err)
 
   console.log('*'.repeat(50))
   console.log(`NebulaNova API Runnig on port: ${process.env.PORT}`)
+  await checkDb()
   console.log('*'.repeat(50))
 })
