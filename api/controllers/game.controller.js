edcc5970aec8286021884d4c4dd029868399be7d
Samuel Falcón Alonso <samu.gc98r@gmail.com>
2024-03-04 16:43:42 +0000

    GAME - API fields changes

--- api/controllers/game.controller.js
+++ api/controllers/game.controller.js
@@ -42,12 +42,12 @@ const createGame = async (req, res) => {
             "Client-ID": process.env.CLIENT_ID,
             Authorization: `Bearer ${process.env.API_TOKEN}`
           },
-          data: `fields *;
+          data: `fields id,name,cover.url;
           where name = ${JSON.stringify(req.body.title)};`
         })
-        console.log(response.data[0].name)
+        console.log(response.data)
         if (response.data.length > 0) {
-          const game = await GameModel.create({ title: response.data[0].name })
+          const game = await GameModel.create({ title: response.data[0].name, image: response.data[0].cover.url })
           return res.status(200).json({ game, message: 'Game created' })
         } else {
           return res.status(404).send('Game not found in external API')
@@ -57,7 +57,7 @@ const createGame = async (req, res) => {
       }
     }
 
-    res.status(200).send('Game already exist')
+    res.status(200).json({ findGame: findGame.title, message: 'Game already exist' })
   } catch (error) {
     console.log(error)
     res.status(500).send('Error creating the game')
@@ -105,3 +105,5 @@ module.exports = {
   updateGame,
   deleteGame
 }
+
+//
