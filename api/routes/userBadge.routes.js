69ae6f96a6ed549bb38b6e06de45a096a97f9dfd
AiramGG <airamguedes99@gmail.com>
2024-03-07 11:00:32 +0000

    BADGES - Added checkAdmin on put route

--- api/routes/userBadge.routes.js
+++ api/routes/userBadge.routes.js
@@ -5,7 +5,7 @@ const { checkAuth, checkAdmin } = require('../middleware/checkAuth.middleware')
 router.get('/', checkAuth, checkAdmin, getAllUserBadges)
 router.get('/:userId/:badgeId', checkAuth, getOneUserBadge)
 router.post('/', checkAuth, checkAdmin, createUserBadge)
-router.put('/:userId/:badgeId', checkAuth, updateUserBadge)
+router.put('/:userId/:badgeId', checkAuth, checkAdmin, updateUserBadge)
 router.delete('/:userId/:badgeId', checkAuth, checkAdmin, deleteUserBadge)
 
 module.exports = router
