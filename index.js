16f9db99247f03c57fff611f26aa94fd68ddd5b0
Samuel Falcón Alonso <samu.gc98r@gmail.com>
2024-04-12 15:21:23 +0100

    comment cors

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const { dbSync, addRelationsToModels } = require('./db/models')
 const app = express()
 
 app.use(cors())
-app.use((req, res, next) => {
+/* app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
   next()
-})
+}) */
 app.use(morgan('dev'))
 app.use(express.json())
 app.use('/api', require('./api/routes/index'))
